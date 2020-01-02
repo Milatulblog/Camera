b38bbec460d6acfa811bbe156a9cd4fec9bd1050
MILA <fafahlatu@gmail.com>
2020-01-03 06:02:39 +0700

    cam

---
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.camera
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+import  android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_capture.setOnClickListener {
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 123)
+
+        }
+
+        img_pick_button.setOnClickListener {
+            var intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 321)
+        }
+    }
+
+    //menampilkan gambar
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            var bmp = data?.extras?.get("data") as? Bitmap
+            foto.setImageBitmap(bmp)
+        }
+        if (requestCode == 321) {
+            foto.setImageURI(data?.data)
+        }
+    }
+
+}
+
+
